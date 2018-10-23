2da8ec2a54a6246cadad896d4d8b0a3f74145862
Marcelo A. Cordeiro <marceloalves6@gmail.com>
2018-10-22 21:52:06 -0300

    Creating

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { TemplateFormComponent } from './template-form/template-form.component';
+import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
 
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'reactive-form', component: ReactiveFormComponent },
+  { path: 'template-form', component: TemplateFormComponent }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TemplateFormComponent,
+    ReactiveFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(routes)
   ],
+  exports: [FormsModule, RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
